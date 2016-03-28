77199faa35ff6720f16835fcecfab05bb8147dd2
kristenvincent <kvincent2@wisc.edu>
2016-03-28 14:59:24 -0500

    Start of Module 9 Activity

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 /* Kristen Vincent's D3 coordinated viz main.js */
 
-//where is topojson.js??
+//code in color scale??
+//generators?
+//on 1.IV
+
+//wrap everything in a self-exectuing anonymous function to move to local scope
+(function() {
+
+//pseudo-global variables
+var attrArray = ["Percent Of Farms With Milk Cows", "How Much Each Cow Is Worth_thousandsofdollarseachcowproducesinayear", "Cows Per Capita", "Active Selling Dairy Farms Per Capita", "Total Farms Per Capita"]; //list of attributes
+var expressed = attrArray[0]; //initial attribute
 
 //begin script when window loads
 window.onload = setMap();
@@ -42,17 +51,84 @@ function setMap() {
 		//translate countyBoundaries topoJSON
 		var wisconsinCounties = topojson.feature(countyBoundaries, countyBoundaries.objects.wicountyboundaries).features;
 
-		//add county boundaries to the map
-		var counties = map.selectAll(".counties")
-			.data(wisconsinCounties)
-			.enter()
-			.append("path")
-			.attr("class", function(d){
-				return d.properties.COUNTY_FIP;
-			})
-			.attr("d", path);
+		//join csv data to GeoJSON enumeration units
+		wisconsinCounties = joinData(wisconsinCounties, csvData);
 
+		//create the color scale
+		var colorScale = makeColorScale(csvData);
 
+		//add enumeration units to the map
+		setEnumerationUnits(wisconsinCounties, map, path, colorScale);
 	};
+}; //end of setMap()
+
+//function to join the data
+function joinData(wisconsinCounties, csvData) {
+	//loop through csv to assign each set of csv attribute values to a geojson region
+	for (var i=0; i<csvData.length; i++) {
+		var csvCounty = csvData[i]; //the current county
+		var csvKey = csvCounty.COUNTY_FIP; //the csv primary key
+
+		//loop through geojson counties to find correct county
+		for (var a=0; a<wisconsinCounties.length; a++) {
+			var geojsonProps = wisconsinCounties[a].properties; //the current county geojson properties
+			var geojsonKey = geojsonProps.COUNTY_FIP; //the geojson primary key
+
+			//where primary keys match, transfer csv data to geojson properties object
+			if (geojsonKey == csvKey) {
+
+				//assign all attributes and values
+				attrArray.forEach(function(attr) {
+					var val = parseFloat(csvCounty[attr]); //get csv attribute value
+					geojsonProps[attr] = val; //assign attribute and value to geojson properties
+				});
+			};
+		};
+	};
+
+	return wisconsinCounties;
+}; //end of joinData function
+
+//function to select counties
+function setEnumerationUnits(wisconsinCounties, map, path) {
+//add county boundaries to the map
+	var counties = map.selectAll(".counties")
+		.data(wisconsinCounties)
+		.enter()
+		.append("path")
+		.attr("class", function(d){
+			return d.properties.COUNTY_FIP;
+		})
+		.attr("d", path);
+}; //end of setEnumerationUnits function
+
+//function to create color scale generator
+function makeColorScale(data) {
+	var colorClasses = [
+		"#f1eef6",
+		"#d7b5d8",
+		"#df65b0",
+		"#dd1c77",
+		"#980043"
+	];
+
+	//create color scale generator
+	var colorScale = d3.scale.quantile()
+		.range(colorClasses);
+
+	//build array of all values of the expressed attribute
+	var domainArray = [];
+	for (var i=0; i<data.length; i++) {
+		var val = parseFloat(data[i][expressed]);
+		domainArray.push(val);
+	};
+
+	//assign array of expressed values as scale domain
+	colorScale.domain(domainArray);
+
+	return colorScale;
 };
+
+
+})(); //end of main.js
 
