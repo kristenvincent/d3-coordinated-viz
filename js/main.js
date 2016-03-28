751bcdd9186f672a96f0fc5c98b889ad36d23301
kristenvincent <kvincent2@wisc.edu>
2016-03-28 13:17:30 -0500

    Module 8 assignment completed

--- js/main.js
+++ js/main.js
@@ -9,21 +9,21 @@ window.onload = setMap();
 function setMap() {
 	//map frame dimensions
 	var width = 960,
-		height = 460;
+		height = 760;
 
 	//create new svg container for the map
 	var map = d3.select("body")
 		.append("svg")
-		.attr("class", map)
+		.attr("class", "map")
 		.attr("width", width)
 		.attr("height", height);
 
 	//create projection
 	var projection = d3.geo.albers()
-		.center([0, 46.2])
-		.rotate([-2, 0, 0])
-		.parallels([43, 62])
-		.scale(25000)
+		.center([0, 44.60])
+		.rotate([90, 0, 0])
+		.parallels([30.64, 44.84])
+		.scale(6500)
 		.translate([width / 2, height / 2]);
 
 	//create a path generator
@@ -31,7 +31,9 @@ function setMap() {
 		.projection(projection);
 
 	//use queue.js to load data all at once
-	d3_queue.queue()
+	q = d3_queue.queue();
+
+	q
 		.defer(d3.csv, "data/D3_Lab_Data.csv")//load attributes from csv
 		.defer(d3.json, "data/wicountyboundaries.topojson")//load county boundaries
 		.await(callback);
@@ -40,17 +42,16 @@ function setMap() {
 		//translate countyBoundaries topoJSON
 		var wisconsinCounties = topojson.feature(countyBoundaries, countyBoundaries.objects.wicountyboundaries).features;
 
-		console.log(wisconsinCounties);
-
 		//add county boundaries to the map
 		var counties = map.selectAll(".counties")
 			.data(wisconsinCounties)
 			.enter()
 			.append("path")
 			.attr("class", function(d){
-				return "counties " + d.properties.COUNTY_FIP;
+				return d.properties.COUNTY_FIP;
 			})
 			.attr("d", path);
+
 
 	};
 };
